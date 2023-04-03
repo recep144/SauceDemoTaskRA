a005e3d72495464376f7ed7a61d6dfe58b61ecf4
recep <recepacar1979@gmail.com>
2023-04-03 18:13:50 +0300

    Task

---
+++ src/test/java/com/krafttech/steps_defs/Hooks.java
@@ -0,0 +1,30 @@
+package com.krafttech.steps_defs;
+
+import com.krafttech.utulities.Driver;
+import io.cucumber.java.Scenario;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.util.concurrent.TimeUnit;
+
+public class Hooks {
+
+    @Before
+    public void setUp(){
+        Driver.get().manage().window().maximize();
+        Driver.get().manage().window().setPosition(new Point(-1000,0));
+        Driver.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown(Scenario scenario){
+        if (scenario.isFailed()){
+            byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+        Driver.closeDriver();
+    }
+}
